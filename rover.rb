2edbd5751a61feae13565600cde0aaf64b38fece
Robert Saunders <robertwsaunders@hotmail.com>
2016-08-14 15:52:45 -0400

    Multiple Rovers Switching Between Rovers

--- rover.rb
+++ rover.rb
@@ -1,22 +1,41 @@
-GRID_SIZE = 20
+
 
 class NavigateRover
+  GRID_SIZE = 20
 
   def self.run
     loop do
       puts "Welcome to the Rover Game!"
-      puts "Please enter the name of your rover:"
-      rover_name = gets.chomp
-      rover = Rover.create(rover_name)
+      puts "Please enter the name(s) of your rover(s): (done)"
+      rover_names = Array.new
       loop do
-        puts "Enter the commands to drive (forward/reverse/left/right/exit):"
-        command = gets.chomp
-        if command == "exit"
-          break
-        else
-          rover.drive(command)
-        end
+        rover = gets.chomp
+        break if rover == "done"
+        rover_names.push(rover)
       end
+      rovers = Array.new
+      rover_names.each do |rover_name|
+        rover = Rover.create(rover_name)
+        rovers.push(rover)
+      end
+      loop do
+        puts "What rover would you like to drive?"
+        driving_rover = gets.chomp
+        if driving_rover == "exit"
+          break
+        end
+          loop do
+            puts "Enter the commands to drive #{driving_rover} (forward/reverse/left/right/switch/exit):"
+            command = gets.chomp
+            if command == "exit"
+              break
+            elsif command == "switch"
+              break
+            else
+              rovers[index(driving_rover)].drive(command)
+            end
+      end
+    end
       puts Rover.instances.inspect
       break
     end
@@ -32,16 +51,28 @@ module Rovers
   #Instance Methods
   def drive(type)
     case type
-    when "left"
-      @location[:x] -= 1 if @location[:x] <= 20
-    when "right"
-      @location[:x] += 1 if @location[:x] <= 20
-    when "forward"
-      @location[:y] += 1 if @location[:y] <= 20
-    when "reverse"
-      @location[:y] -= 1 if @location[:y] <= 20
+    when "left","l"
+      perform_maneuver(:x,"-")
+    when "right","r"
+      perform_maneuver(:x,"+")
+    when "forward","f"
+      perform_maneuver(:y,"+")
+    when "reverse","re"
+      perform_maneuver(:y,"-")
     else
       puts "This drive command is not supported."
+    end
+  end
+
+  def perform_maneuver(key,direction)
+    if @location[key] < NavigateRover::GRID_SIZE
+      if direction == "+"
+        @location[key] += 1
+      else
+        @location[key] -= 1
+      end
+    else
+      puts "Grid restriction, cannot move here."
     end
   end
 
